e371b94eac8fe0e4d62bb08b9b26ef5d8ec20dc4
Steven <steve196t@gmail.com>
2025-03-01 16:59:44 -0600

    Add cache clear step on deploy and remove base paths

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,6 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   output: isProd ? "export" : undefined,
-  basePath: isProd ? "/website-v2" : undefined,
-  assetPrefix: isProd ? "/website-v2" : undefined,
   trailingSlash: true,
 };
 
